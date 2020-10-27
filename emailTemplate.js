1950332157711db9893ed33907480c4f748abac8
TusharMazhar <tushar.talukdar@northsouth.edu>
2020-10-27 16:12:53 +0600

    template done

--- emailTemplate.js
+++ emailTemplate.js
@@ -1,3 +1,203 @@
 const list=()=>{
+
+    var template=`<!DOCTYPE html >
+
+    <head>
+    <meta content="en-us" http-equiv="Content-Language" />
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+    <title></title>
+    </head>
+    
+    <body >
+    
+    <table style="background-color: #ffffff;" align="center" bgcolor="#FFFFFF" border="0" cellpadding="0" cellspacing="0" width="650px"style="margin-top:60px">
+        <tr>
+            <td>
+            <table align="center" border="0" cellpadding="0" cellspacing="0" width="700">
+                <tr>
+                    <td valign="top">
+                    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                        <tr>
+                            <td width="20">&nbsp;</td>
+                            <td height="60" style="font-size: 14px; font-family: Arial; text-align: center; line-height: 20px;font-weight: bold; color:red; border-bottom: 1px solid #f08909;" valign="middle">
+                            Please do not reply to this message. Replies to this 
+                            message are routed to an unmonitored mailbox. If you 
+                            have questions, <span style="color:green"><strong>please call us at +022-36537647</strong></span></td>
+                            <td width="20">&nbsp;</td>
+                        </tr>
+                    </table>
+                    </td>
+                </tr>
+                <tr>
+                    <!-- <td valign="top"> -->
+                    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                        <tr style="text-align: center;">
+                            <td width="20">&nbsp;</td>
+                            <td height="140" valign="middle">
+                            <img alt="IMCicon" src="IMCicon.jpg" /></td>
+                            <td width="20">&nbsp;</td>
+                        </tr>
+                    </table>
+                    <!-- </td> -->
+                </tr>
+                <!-- <tr>
+                    <td height="40" style="font-size: 0; line-height: 0;">&nbsp;</td>
+                </tr> -->
+                <tr>
+                    <td>
+                    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                        <tr>
+                            <td width="20">&nbsp;</td>
+                            <td valign="top">
+                            <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                <tr style="text-align:center;">
+                                    <td style="font-family: Arial; font-size: 24px; line-height: 24px; color: #3786AB;">
+                                    <span style="font-size:2rem;font-weight:bold;text-align:center;" >Welcome To InNeed Managed Cloud</span> </td>
+                                </tr>
+                                <tr>
+                                    <td height="30" style="font-size: 0; line-height: 0;">
+                                    &nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td style="font-family: Arial; font-size: 14px; line-height: 20px; color: #000;">
+                                    Dear: ${name}</td>
+                                </tr>
+                                <tr>
+                                    <td height="20" style="font-size: 0; line-height: 0;">
+                                    &nbsp;</td>
+                                </tr>
+                                <tr >
+                                    <td style="font-family: Arial; font-size: 14px; line-height: 20px; color: #000;text-align: justify;">
+                                        Thank you for creating a <span style="font-weight: bold;">IMC</span> account.You're all ready to go!! <br> <br>
+                                        IMC, formally known as InNeed Managed Cloud, is basically cloud services provider. It provides a range of cloud services, including compute, analytics, storage and networking.You can pick and choose from these services to develop and scale new applications, or run existing applications in the public cloud. <br> <br>
+                                        Not only that, If you want to migrate your applications to the big cloud providers like <span style="font-weight: bold;">AWS, AZURE, IBM, GOOGLE </span>clouds,you don't have to worry about that.We are providing all kinds of services for you.We will help you to migrate your apllications to the big cloud platforms.
+    
+    
+                                </tr>
+                                <tr>
+                                    <td height="20" style="font-size: 0; line-height: 0;">
+                                    &nbsp;</td>
+                                
+                                </tr>
+                                <tr>
+                                    <td style="font-family: Arial; font-size: 14px; line-height: 20px; color: #000;">
+                                        For more Information about InNeed Managed Cloud,Please go to <span style="font-size: 1rem;color:green;font-weight: bold;"><a href="https://inneed.cloud/">https://inneed.cloud/</a></span></td>
+                                </tr >
+                                <tr>
+                                    <td height="25" style="font-size: 0; line-height: 0;">
+                                    &nbsp;</td>
+                                </tr>
+                                    
+                                    <td style="font-family: Arial; font-size: 14px; line-height: 20px; color: #000;">
+                                    Thank you!</td>
+                                </tr>
+                                
+                                <tr>
+                                    <td style="font-family: Arial; font-size: 14px; line-height: 20px; color: #000;">
+                                
+                                    InNeed Managed Cloud Team</td>
+                                </tr>
+                                <tr>
+                                    <td height="40" style="font-size: 0; line-height: 0;">
+                                    &nbsp;</td>
+                                </tr>
+                            </table>
+                            </td>
+                            <td width="20">&nbsp;</td>
+                        </tr>
+                    </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="border-top: 1px solid #f08909; border-bottom: 1px solid #f08909;" valign="top">
+                    <table border="0" cellpadding="0" cellspacing="0" width="100%" >
+                        <tr>
+                            <td width="20">&nbsp;</td>
+                            <td valign="top">
+                            <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                <tr>
+                                    <td height="8" style="font-size: 0; line-height: 0;">
+                                    </td>
+                                    <td height="8" style="font-size: 0; line-height: 0;">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td valign="top">
+                                    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                        <tr>
+                                            <td style="font-family: Arial; font-size: 14px; line-height: 20px; color: #000;">
+                                            
+                                        </tr>
+                                        <tr>
+                                            <td height="10" style="font-size: 0; line-height: 0;">
+                                            &nbsp;</td>
+                                        </tr>
+    
+                                        <tr>
+                                            <span style="color:rgb(87, 75, 75)">
+                                                <b>USA Head Office:</b><br>
+                                                1601 Elm st, Dallas, TX 75201 <br> <br>
+                                            </span>  
+                                            <span style="color:rgb(87, 75, 75)">
+    
+    
+                                                <b>Bangladesh Corporate Office:</b> <br> 
+                                                Flat 8-M, KA-90, Progoti Sharani, Kuril, Dhaka 1229,
+                                                Bangladesh <br>
+                                            </span> 
+                                            <!-- <span>
+                                                Bangladesh Marketing Office <br>
+                                                1st Floor, House 5/4/8, Block A, Lalmatia (Water Tank Road),
+                                                Dhaka 1207, Bangladesh <br>
+                                            </span>  -->
+    
+    
+                                            <td >
+                                            <a href="https://twitter.com/" style="text-decoration: none;" target="_blank">
+                                            <img alt="Twitter" border="0" src="https://www.julyservices.com/unifyOnlineImages/email/twitter.jpg" /></a>
+                                            <a href="https://www.facebook.com/InNeed.Cloud" style="text-decoration: none;" target="_blank">
+                                            <img alt="facebook" border="0" src="https://www.julyservices.com/unifyOnlineImages/email/facebook.jpg" /></a>
+                                            <a href="https://www.linkedin.com/company/inneed-cloud/" style="text-decoration: none;" target="_blank">
+                                            <img alt="LinkedIn" border="0" src="https://www.julyservices.com/unifyOnlineImages/email/linkedin.jpg" /></a></td>
+                                        </tr>
+                                    </table>
+                                    </td>
+                                    <td align="right" style="font-family: Arial; font-size: 14px; line-height: 18px; color: #000;" valign="top">
+                                    +022-37638373 Team <br />
+                                    contact@inneed.cloud <br>
+                                    <a href="https://inneed.cloud/" style="color: #145b9d; text-decoration: none;" target="_blank">
+                                        https://inneed.cloud/</a> </td>
+                                </tr>
+                                <tr>
+                                    <td height="5" style="font-size: 0; line-height: 0;">
+                                    </td>
+                                    <td height="5" style="font-size: 0; line-height: 0;">
+                                    </td>
+                                
+                                </tr>
+                            </table>
+                            </td>
+                        
+                        </tr>
+                    </table>
+                    </td>
+                </tr>
+            </table>
+            </td>
+        </tr>
+    </table>
+    
+    </body>
+    
+    </html>
+    
+    
+    
+    
+    
+    
+    
+    
+    `
     
 }
